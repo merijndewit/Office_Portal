d204a17d725ad290c6240a9ddc6c4dc4ee98d6c2
merijndewit <merijn.de.wit@gmail.com>
2021-05-21 15:09:27 +0100

    gsteamer and raspivid now gets closed on exit of program

--- Office_Portals_Start_Streaming.py
+++ Office_Portals_Start_Streaming.py
@@ -1,14 +1,16 @@
 import subprocess
+import os
+import signal
 
 stream = None
-
 def stream():
-    global stream 
+    global stream
     stream = subprocess.Popen( "raspivid -t 999999 -b 2000000 -o - -n| gst-launch-1.0 -e -vvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host=192.168.1.139 port=5000",
-    stdin=subprocess.PIPE, shell=True )
+    stdin=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+    print("started stream")
     
 def stopstream():
     global stream
-    stream.terminate()
+    os.killpg(os.getpgid(stream.pid), signal.SIGTERM)
     stream = None
     print("stream stopped")
