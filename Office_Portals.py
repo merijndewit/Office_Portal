561f331b985e364a588ebcf96736ffa90bbccdf8
merijndewit <merijn.de.wit@gmail.com>
2021-05-25 11:27:44 +0100

    now able to edit ip adress with string

--- Office_Portals.py
+++ Office_Portals.py
@@ -1,6 +1,8 @@
 import PySimpleGUI as gui
 import Office_Portals_Start_Streaming as ststream
 
+ststream.ip = "192.168.1.139" #set the target ip
+
 layout = [[gui.Text('Office Portals', font=("Helcentica", 20))],
           [gui.Text('A nerdy solution to make distant colleagues feel nearby', font=("Helcentica", 12), s = (100, 12))],
             [gui.Button("Start Streaming")],
@@ -16,6 +18,6 @@ while True:
     if event in (gui.WIN_CLOSED, 'Exit'):
         ststream.stopstream()
         break
-    
-    
+
+
 window.close()
--- Office_Portals_Start_Streaming.py
+++ Office_Portals_Start_Streaming.py
@@ -2,13 +2,15 @@ import subprocess
 import os
 import signal
 
+
 stream = None
 def stream():
     global stream
-    stream = subprocess.Popen( "raspivid -t 999999 -b 2000000 -o - -n| gst-launch-1.0 -e -vvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host=192.168.1.139 port=5000",
-    stdin=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+    p1 = subprocess.Popen(["raspivid", "-t", "999999", "-b", "2000000", "-o", "-",], stdout=subprocess.PIPE)
+    stream = subprocess.Popen(["gst-launch-1.0", "-e", "-vvv", "fdsrc", "!", "h264parse", "!", "rtph264pay", "pt=96", "config-interval=5", "!", "udpsink", "host=", ip ,"port=","5000"], stdin=p1.stdout, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+    p1.stdout.close()
     print("started stream")
-    
+
 def stopstream():
     global stream
     os.killpg(os.getpgid(stream.pid), signal.SIGTERM)
