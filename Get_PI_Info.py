28cf38edabdd8b8aeb3de8d054af3d360a776fb6
merijndewit <merijn.de.wit@gmail.com>
2021-05-25 14:38:15 +0100

    program now shows pi's ip address

---
+++ Get_PI_Info.py
@@ -0,0 +1,7 @@
+import socket
+import netifaces as ni
+
+def getip():
+    ni.ifaddresses('wlan0')
+    ip = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
+    return(ip)
--- Office_Portals.py
+++ Office_Portals.py
@@ -1,10 +1,12 @@
 import PySimpleGUI as gui
 import Office_Portals_Start_Streaming as ststream
+import Get_PI_Info as getpi
 
 layout = [[gui.Text('Office Portals', font=("Helcentica", 20))],
           [gui.Text('A nerdy solution to make distant colleagues feel nearby', font=("Helcentica", 12), size = (100, 1))],
-           [gui.Text('Target IP', font = ("Helcentica", 9)),gui.Input(key = 'targetip', size = (20,1))],
-            [gui.Button("Start Streaming")],
+           [gui.Text('PI`s IP:'),gui.Text(getpi.getip())],
+            [gui.Text('Target IP:', font = ("Helcentica", 9)),gui.Input(key = 'targetip', size = (20,1))],
+             [gui.Button("Start Streaming")],
          [gui.Exit()]]
 window = gui.Window('Office Portals', layout, size = (480,360),resizable = True , element_justification = "center", finalize = True)
 #window.Maximize()
