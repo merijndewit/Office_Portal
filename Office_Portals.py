12e094032f3da0f4a4b5e0e86a91bc3a53cf2d40
merijndewit <merijn.de.wit@gmail.com>
2021-05-26 15:07:34 +0100

    now able to stop and start the stream

--- Office_Portals.py
+++ Office_Portals.py
@@ -1,27 +1,37 @@
 import PySimpleGUI as gui
 import Office_Portals_Start_Streaming as ststream
+import Office_Portals_Recieve_Stream as rcstream
 import Get_PI_Info as getpi
 
 layout = [[gui.Text('Office Portals', font=("Helcentica", 20))],
           [gui.Text('A nerdy solution to make distant colleagues feel nearby', font=("Helcentica", 12))],
            [gui.Text('PI`s IP:'),gui.Text(getpi.getip())],
-            [gui.Text('Target IP:', font = ("Helcentica", 9)),gui.Input(key = 'targetip', size = (20,1))],
+            [gui.Text('Target IP:', font = ("Helcentica", 9)),gui.Input(key='targetip', size = (20,1))],
              [gui.Text('PI camera CSI/USB'), gui.Checkbox(key = ("picam"), text = 'unchecked = CSI') ],
               [gui.Text('Target PI camera CSI/USB'), gui.Checkbox(key = ("targetpicam"), text = 'unchecked = CSI')],
-               [gui.Button("Start Streaming")],
+               [gui.Button("Start Streaming"), gui.Button("Stop Streaming")],
+                [gui.Button("Start Recieving Stream"), gui.Button("Stop Revieving Stream")],
          [gui.Exit()]]
 window = gui.Window('Office Portals', layout, size = (640,480),resizable = True , element_justification = "center", finalize = True)
-#window.Maximize()
+# window.Maximize()
 
 while True:
     event, values = window.read()
     print(event, values)
     if event == 'Start Streaming':
-        piCSI = values['picam']
+        ststream.piCSI = values['picam']
         ststream.ip = values['targetip']
-        ststream.stream(piCSI)
+        ststream.stream()
+    if event == 'Start Recieving Stream':
+        targetpiCSI = values['targetpicam']
+        rcstream.recieveStream(targetpiCSI)
+    if event == 'Stop Streaming':
+        ststream.stopstream()
+    if event == 'Stop Revieving Stream':
+        rcstream.stopreceivingstream()
     if event in (gui.WIN_CLOSED, 'Exit'):
         ststream.stopstream()
+        rcstream.stopreceivingstream()
         break
 
 
--- Office_Portals_Start_Streaming.py
+++ Office_Portals_Start_Streaming.py
@@ -1,24 +1,28 @@
 import subprocess
 import os
-import signal
 
-stream = None
-def stream(piCSI):
-    global stream
-    if piCSI == False:
-        p1 = subprocess.Popen(["raspivid", "-t", "999999", "-b", "2000000", "-o", "-",], stdout=subprocess.PIPE)
-        stream = subprocess.Popen(["gst-launch-1.0", "-e", "-vvv", "fdsrc", "!", "h264parse", "!", "rtph264pay", "pt=96", "config-interval=5", "!", "udpsink", "host=", ip ,"port=","5000"], stdin=p1.stdout, stdout=subprocess.PIPE, preexec_fn=os.setsid)
-        p1.stdout.close()
+piCSI = None
+
+def stream():
+    global _stream
+    _stream = None
+    if piCSI is False:
+        _p1 = subprocess.Popen(["raspivid", "-t", "999999", "-b", "2000000", "-o", "-",], stdout=subprocess.PIPE)
+        _stream = subprocess.Popen(["gst-launch-1.0", "-e", "-vvv", "fdsrc", "!", "h264parse", "!", "rtph264pay", "pt=96", "config-interval=5", "!", "udpsink", "host=", ip ,"port=","5000"], preexec_fn=os.setsid)
+        _p1.stdout.close()
         print("started stream for CSI camera")
-    elif piCSI == True:
-        stream = subprocess.Popen(["gst-launch-1.0", "-vv", "-e", "v4l2src", "device=/dev/video0", "!",  "image/jpeg,width=640,height=480", "!", "queue", "!", "jpegdec", "!", "videoconvert", "!", "omxh264enc", "target-bitrate=1000000", "control-rate=variable", "!", "h264parse", "!", "rtph264pay", "config-interval=1", "pt=96", "!", "udpsink", "host=", ip, "port=5000"], stdout=subprocess.PIPE, preexec_fn=os.setsid)
+    elif piCSI is True:
+        _stream = subprocess.Popen(["gst-launch-1.0", "-vv", "-e", "v4l2src", "device=/dev/video0", "!",  "image/jpeg,width=640,height=480", "!", "queue", "!", "jpegdec", "!", "videoconvert", "!", "omxh264enc", "target-bitrate=1000000", "control-rate=variable", "!", "h264parse", "!", "rtph264pay", "config-interval=1", "pt=96", "!", "udpsink", "host=", ip, "port=5000"], preexec_fn=os.setsid)
         print("started stream for USB camera")
     else:
         return(0)
 
 def stopstream():
-    global stream
-    if hasattr(stream, 'pid'):
-        os.killpg(os.getpgid(stream.pid), signal.SIGTERM)
-        stream = None
-    print(stream)
+    global _stream
+    #os.killpg(os.getpgid(stream.pid), signal.SIGTERM)
+
+    subprocess.Popen.kill(_stream)
+    _p1 = None
+    _stream = None
+
+    print("stream termindated")
