8febea1660bca40783e4f188f50323ec07803dac
merijndewit <merijn.de.wit@gmail.com>
2021-06-02 16:11:16 +0100

    user is now able to type the targer resolution and framerate

--- Office_Portals.py
+++ Office_Portals.py
@@ -8,12 +8,13 @@ layout = [[gui.Text('Office Portals', font=("Helcentica", 20))],
           [gui.Text('A nerdy solution to make distant colleagues feel nearby', font=("Helcentica", 12))],
            [gui.Text('PI`s IP:'),gui.Text(getpi.getip())],
             [gui.Text('Target IP:', font = ("Helcentica", 9)),gui.Input(key='targetip', size = (20,1))],
-             [gui.Text('PI camera CSI/USB'), gui.Checkbox(key = ("picam"), text = 'unchecked = CSI (only Gstreamer)') ],
-              [gui.Text('Target PI camera CSI/USB'), gui.Checkbox(key = ("targetpicam"), text = 'unchecked = CSI (only Gstreamer)')],
-               [gui.Text('Color'), gui.Checkbox(key = ("ledcolor"), text = 'unchecked = Orange')],
-                [gui.Text('Status:'), gui.Text('',key='Statustext')],
-                 [gui.Button("Start Streaming"), gui.Button("Stop Streaming")],
-                  [gui.Button("Start Recieving Stream"), gui.Button("Stop Revieving Stream")],
+             [gui.Text('Target pixel width:', font = ("Helcentica", 9)),gui.Input(key='targetwidth', size = (20,1))],
+              [gui.Text('Target pixel height:', font = ("Helcentica", 9)),gui.Input(key='targetheight', size = (20,1))],
+               [gui.Text('Target fps:', font = ("Helcentica", 9)),gui.Input(key='targetfps', size = (20,1))],
+                [gui.Text('Color'), gui.Checkbox(key = ("ledcolor"), text = 'unchecked = Orange')],
+                 [gui.Text('Status:'), gui.Text('',key='Statustext')],
+                  [gui.Button("Start Streaming"), gui.Button("Stop Streaming")],
+                   [gui.Button("Start Recieving Stream"), gui.Button("Stop Revieving Stream")],
          [gui.Exit()]]
 window = gui.Window('Office Portals', layout, size = (640,480),resizable = True , element_justification = "center", finalize = True)
 # window.Maximize()
@@ -25,8 +26,10 @@ while True:
     if event == 'Start Streaming':
         color = values['ledcolor']
         led.setcolor(color)
-        ststream.piCSI = values['picam']
         ststream.ip = values['targetip']
+        ststream.vidwidth = values['targetwidth']
+        ststream.vidheight = values['targetheight']
+        ststream.vidfps = values['targetfps']
         ststream.stream()
     if event == 'Start Recieving Stream':
         targetpiCSI = values['targetpicam']
--- Office_Portals_Start_Streaming.py
+++ Office_Portals_Start_Streaming.py
@@ -6,14 +6,8 @@ piCSI = None
 def stream():
     global _stream
     _stream = None
-    if piCSI is False:
-        _stream = subprocess.Popen(["gst-launch-1.0","rpicamsrc","preview=False","name=videosrc","bitrate=4000000","!","h264parse","!","video/x-h264,framerate=20/1,width=1920,height=1080","!","rtph264pay","pt=96","config-interval=5","!","udpsink","host=",ip,"port=5000"], preexec_fn=os.setsid)
-        print("started stream for CSI camera")
-    elif piCSI is True:
-        #_stream = subprocess.Popen(["gst-launch-1.0", "-vv", "-e", "v4l2src", "device=/dev/video0", "!",  "image/jpeg,width=640,height=480", "!", "queue", "!", "jpegdec", "!", "videoconvert", "!", "omxh264enc", "target-bitrate=1000000", "control-rate=variable", "!", "h264parse", "!", "rtph264pay", "config-interval=1", "pt=96", "!", "udpsink", "host=", ip, "port=5000"], preexec_fn=os.setsid)
-        print("not available yet")
-    else:
-        return(0)
+    _stream = subprocess.Popen(["gst-launch-1.0","rpicamsrc","preview=False","name=videosrc","bitrate=4000000","!","h264parse","!","video/x-h264,framerate=",vidfps + "/1,","width=",vidwidth,",height=",vidheight,"!","rtph264pay","pt=96","config-interval=5","!","udpsink","host=",ip,"port=5000"], preexec_fn=os.setsid)
+    print("started stream for CSI camera")
 
 def stopstream():
     global _stream
