8da7df62cf169cea4a81dd21ac0d32734370853d
merijndewit <merijn.de.wit@gmail.com>
2021-06-10 13:55:48 +0100

    fixed layout and install command for rpicamsrc

--- GUI/newGUI/Install_Dependencies.py
+++ GUI/newGUI/Install_Dependencies.py
@@ -7,8 +7,7 @@ def installGstreamertools():
     
 def installRpicamsrc():
     global _stream
-    _stream = subprocess.Popen(["sudo apt-get install autoconf && git clone https://github.com/thaytan/gst-rpicamsrc.git && ./gst-rpicamsrc/autogen.sh --prefix=/usr --libdir=/usr/lib/arm-linux-gnueabihf/ && gst-rpicamsrc/make && sudo gst-rpicamsrc/make install"], shell=True)
-    
+    _stream = subprocess.Popen(["git clone https://github.com/thaytan/gst-rpicamsrc.git && cd gst-rpicamsrc && ./autogen.sh --prefix=/usr --libdir=/usr/lib/arm-linux-gnueabihf/ && make && sudo make install"], shell=True, cwd='/home/pi')
 def installGstreamerdev():
     global _stream
     _stream = subprocess.Popen(["sudo apt-get install autoconf automake libtool pkg-config libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libraspberrypi-dev"], shell=True)
