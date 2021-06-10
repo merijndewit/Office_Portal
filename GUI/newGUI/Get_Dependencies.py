b01208e5c1a6a6fb757699acdec60bffc9ad0894
merijndewit <merijn.de.wit@gmail.com>
2021-06-10 09:59:50 +0100

    now able to look for rpicamsrc plugin

--- GUI/newGUI/Get_Dependencies.py
+++ GUI/newGUI/Get_Dependencies.py
@@ -18,7 +18,21 @@ def checkGstreamer():
         #print("gstreamer is not installed")
         return(0)
 
-    
+def checkRpicamsrc():
+    #check if rpicamsrc is installed
+    global _checkGstreamer
+    out = check_output(["gst-inspect-1.0", "rpicamsrc"])
+    encoding = 'utf-8'
+    encodedOutput = (str(out, encoding))
+    o = encodedOutput.find("Version") #here we look in the output for "Version" because it will only show when rpicamsrc is installed and that is what we want to know
+    if o != -1:
+        #print("rpicamsrc is installed")
+        return(1)
+    else:
+        #print("rpicamsrc is not installed")
+        return(0)
  
+
+
  
-checkGstreamer()
+checkRpicamsrc()
--- GUI/newGUI/main_Office_Portal.py
+++ GUI/newGUI/main_Office_Portal.py
@@ -6,7 +6,7 @@ gui.theme('Default1')
 staticLayout = [[gui.Column(Layouts.Introduction, key='-PG0-'), gui.Column(Layouts.Dependencies, visible=False, key='-PG1-')],
           [gui.Button(key='prevPage', image_filename='Pictures/arrow_left.png', border_width=0), gui.Button(key='Exit', image_filename='Pictures/Exit_Button.png', border_width=0),gui.Button(key='nextPage', image_filename='Pictures/arrow_right.png', border_width=0)]]
 
-window = gui.Window('Office Portals', staticLayout, size = (640,480),resizable = False , element_justification = "center")
+window = gui.Window('Office Portals', staticLayout, size = (640,480),resizable = False , element_justification="center")
 
 staticLayout = 0
 while True:
@@ -34,6 +34,14 @@ while True:
         else:
             #gstreamer-tools is installed
             window['gstreamer-toolsInstalled'].update(visible=True)
+
+    if event == 'checkRpicamsrc':
+        if getdp.checkGstreamer() == 0: #the function returns a 1 or a 0. 0 for when gstreamer-tools is not installed and 1 for when it is.
+            #gstreamer-tools not installed
+            window['installRpicamsrc'].update(visible=True)
+        else:
+            #gstreamer-tools is installed
+            window['rpicamsrcInstalled'].update(visible=True)
 
 
 window.close()
