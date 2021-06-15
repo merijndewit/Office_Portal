5fa680b232021b5945700dcf949a2492b836d7c7
merijndewit <merijn.de.wit@gmail.com>
2021-06-15 12:36:45 +0100

    program now updates gstreamer-tools if its not updated by debian bbuster

--- GUI/Source_Code_OfficePortal-0.1/Get_Dependencies.py
+++ GUI/Source_Code_OfficePortal-0.1/Get_Dependencies.py
@@ -6,15 +6,17 @@ import time
 def checkGstreamer():
     #check if gstreamer is installed
     #_checkGstreamer = subprocess.Popen(["sudo apt show gstreamer1.0-tools"], shell=True, preexec_fn=os.setsid, stdout=subprocess.PIPE)
-    out = check_output(["sudo", "apt", "show", "gstreamer1.0-tools"])
-    encoding = 'utf-8'
-    encodedOutput = (str(out, encoding))
-    o = encodedOutput.find("Version") #here we look in the output for "Version" because it will only show when gstreamer is installed and that is what we want to know
-    if o != -1:
-        #print("gstreamer is installed")
-        return(1)
-    else:
-        #print("gstreamer is not installed")
+    try:
+        outinspect = check_output(["gst-inspect-1.0"])
+        out = check_output(["sudo", "apt", "show", "gstreamer1.0-tools"])
+        encoding = 'utf-8'
+        encodedOutput = (str(out, encoding))
+        o = encodedOutput.find("Version") #here we look in the output for "Version" because it will only show when gstreamer is installed and that is what we want to know
+        if o != -1:
+            return(1)
+        else:
+            return(0)
+    except:
         return(0)
 
 def checkRpicamsrc():
