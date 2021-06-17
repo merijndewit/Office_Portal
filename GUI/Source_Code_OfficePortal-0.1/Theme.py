8cda62a0f80354c328d0a95d83125a0bac3c1d45
merijndewit <merijn.de.wit@gmail.com>
2021-06-17 09:21:23 +0100

    changes theme to fully white

--- GUI/Source_Code_OfficePortal-0.1/Theme.py
+++ GUI/Source_Code_OfficePortal-0.1/Theme.py
@@ -3,10 +3,10 @@ mouseOver = '#dcf9fc'
 def setTheme():
     return(
         gui.theme('Default1'),
-        gui.theme_background_color('#ebfdff'),
+        gui.theme_background_color('white'),
         #gui.theme_border_width('#ebfdff'),
-        gui.theme_button_color('#ebfdff'),
-        gui.theme_element_background_color('#ebfdff'),
+        gui.theme_button_color('white'),
+        gui.theme_element_background_color('white'),
         #gui.theme_element_text_color('#ebfdff'),
         gui.theme_input_background_color('white')
         #gui.theme_input_text_color('#ebfdff'),
