c95d7c234e1e6223ee8632eb12fd60bf499bb50b
merijndewit <merijn.de.wit@gmail.com>
2021-06-21 15:47:23 +0100

    changed from spi to pwm because spi is not stable

--- GUI/Source_Code_OfficePortal-0.1/LED.py
+++ GUI/Source_Code_OfficePortal-0.1/LED.py
@@ -1,13 +1,15 @@
 import board
 import neopixel
-pixels = neopixel.NeoPixel(board.D10, 13)
+pixels = neopixel.NeoPixel(board.D18, 13)
 
 def setcolor():
+    global pixels
     with open('office_portal.txt') as f:
         configLines = [ line.strip() for line in f ]
+    
     if configLines[11] == 'True':
-        pixels.fill((137, 207, 240))
-        print('ledBlue')
+        pixels.fill((0, 100, 0))
     else:
-        pixels.fill((255, 92, 0))
+        pixels.fill((100, 0, 0))
         print('ledOrange')
+    
