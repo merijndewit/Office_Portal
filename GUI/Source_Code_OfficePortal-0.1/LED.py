9e61785023f9054e9887b8ca41198a25a59e29f0
merijndewit <merijn.de.wit@gmail.com>
2021-06-22 14:16:58 +0100

    now able to use custom color

--- GUI/Source_Code_OfficePortal-0.1/LED.py
+++ GUI/Source_Code_OfficePortal-0.1/LED.py
@@ -1,3 +1,4 @@
+from typing import Awaitable
 import board
 import neopixel
 pixels = neopixel.NeoPixel(board.D18, 13)
@@ -7,12 +8,12 @@ def setcolor():
     with open('office_portal.txt') as f:
             configLines = [ line.strip() for line in f ]
     pixels.brightness = float(configLines[16])
-    with open('office_portal.txt') as f:
-        configLines = [ line.strip() for line in f ]
-    
-    if configLines[11] == 'True':
-        pixels.fill((0, 100, 0))
+    if configLines[15] == 'True':
+        pixels.fill((int(configLines[17]), int(configLines[18]), int(configLines[19])))
     else:
-        pixels.fill((100, 0, 0))
-        print('ledOrange')
+        if configLines[11] == 'True':
+            pixels.fill((0, 179, 255))
+        else:
+            pixels.fill((255, 60, 0))
+            print('ledOrange')
     
