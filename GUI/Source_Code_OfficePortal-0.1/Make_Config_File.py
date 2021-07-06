f02173c0dccaed5a4f514adee36819971f72822b
merijndewit <merijn.de.wit@gmail.com>
2021-07-06 13:50:44 +0100

    removed autostartup fom config

--- GUI/Source_Code_OfficePortal-0.1/Make_Config_File.py
+++ GUI/Source_Code_OfficePortal-0.1/Make_Config_File.py
@@ -6,11 +6,6 @@ if getattr(sys, 'frozen', False):
 else:
     cd = os.path.dirname(os.path.abspath(__file__))
 
-def clearConfigfile():
-    #f = open(cd + '/office_portal.txt', "w+")
-    #f.truncate(0)
-    #f.close()
-    return(0)
 def makeConfig(values, configspecs):
     config = {}
     config['streamInfo'] = []
@@ -22,21 +17,20 @@ def makeConfig(values, configspecs):
     'ledStrip': values[configspecs[4]],
     'ledTexture': values[configspecs[5]],
     'none': values[configspecs[6]],
-    'autostart': values[configspecs[7]],
-    'bitrate': values[configspecs[8]],
-    'portSend': values[configspecs[9]],
-    'portReceive': values[configspecs[10]],
-    'blue': values[configspecs[11]],
-    'orange': values[configspecs[12]],
-    'ring1080': values[configspecs[13]],
-    'ring720': values[configspecs[14]],
-    'customcolor': values[configspecs[15]],
-    'brightness': values[configspecs[16]],
-    'Rvalue': values[configspecs[17]],
-    'Gvalue': values[configspecs[18]],
-    'Bvalue': values[configspecs[19]],
-    'customTexture': values[configspecs[20]],
-    'customPath': values[configspecs[21]]
+    'bitrate': values[configspecs[7]],
+    'portSend': values[configspecs[8]],
+    'portReceive': values[configspecs[9]],
+    'blue': values[configspecs[10]],
+    'orange': values[configspecs[11]],
+    'ring1080': values[configspecs[12]],
+    'ring720': values[configspecs[13]],
+    'customcolor': values[configspecs[14]],
+    'brightness': values[configspecs[15]],
+    'Rvalue': values[configspecs[16]],
+    'Gvalue': values[configspecs[17]],
+    'Bvalue': values[configspecs[18]],
+    'customTexture': values[configspecs[19]],
+    'customPath': values[configspecs[20]]
     }
     with open(cd + '/config.txt', 'w+') as outfile:
         json.dump(config, outfile)
